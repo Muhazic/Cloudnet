1bb0ebab0bd68b9a92fa714d256f020b8fe74384
Muhamed Fejzic <91850725+Muhazic@users.noreply.github.com>
2023-10-01 14:27:38 -0500

    Add files via upload

---
+++ Cloudnet/providers.tf
@@ -0,0 +1,61 @@
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "default-mf"
+    workspaces {
+      name = "Cloudnet"
+    }
+  }
+
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.18.1" 
+      
+    }
+  }
+}
+
+provider "aws" {
+  alias  = "primary"
+  region = var.primary_region
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+}
+
+provider "aws" {
+  alias  = "secondary"
+  region = var.secondary_region
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+}
+
+
+/*
+Switch backend to terraform cloud
+
+backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "default-mf"
+    workspaces {
+      name = "workspace1"
+    }
+  }
+  
+
+Swtich to s3 backend
+
+backend "s3" {
+    bucket = "terraform-state-file-mf37" // tf state will be stored here
+    key    = "web_host/resume/ec2/infra"
+    //region = var.primary_region
+  }
+
+Switch to local
+
+backend "local" {
+    path = "terraform.tfstate"
+  }
+
+*/
